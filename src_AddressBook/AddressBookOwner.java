dbaf542bab65e3a1c32dd34bc2976598356975ea
ashu3902 <moreashutosh1997@gmail.com>
2020-11-01 23:19:27 +0530

    Added: Address Book implementation

---
+++ src_AddressBook/AddressBookOwner.java
@@ -0,0 +1,11 @@
+package AddressBook;
+
+public interface AddressBookOwner {
+	public void newAddressBook();
+	public void openAddressBook();
+	public void closeAddressBook();
+	public void saveAddressBook();
+	public void quit();
+	public void saveasAddressBook();
+
+}
